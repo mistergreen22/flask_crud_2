c12b650663ee4535d2d368ead7c8e627b087e9ab
Pavlo Havrylov <greenpasha22@gmail.com>
2021-02-19 14:28:25 +0200

    Add update and delete methods

--- app/blueprints/users.py
+++ app/blueprints/users.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from app.models import user, connection, to_json
-from sqlalchemy import text
 
 users = Blueprint('users', __name__)
 
@@ -29,3 +28,29 @@ def read_one(user_id):
     if o is None:
         return '', 404
     return jsonify(to_json(o))
+
+
+@users.route('/<user_id>', methods=['PUT', 'PATCH'])
+def update(user_id):
+    u = user.update().where(
+        user.columns.id == user_id
+    ).values(first_name=request.json['first_name'],
+             last_name=request.json['last_name'],
+             age=request.json['age'],
+             email=request.json['email'])
+    o = connection.execute(u)
+    if o is None:
+        return '', 404
+    return read_one(user_id)
+
+
+@users.route('/<user_id>', methods=['DELETE'])
+def delete(user_id):
+    u = user.delete().where(user.columns.id == user_id)
+    o = connection.execute(u)
+    if o.rowcount == 0:
+        return '', 404
+    return '', 204
+
+
+
